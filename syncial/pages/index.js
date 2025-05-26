b61b8e4354c222f5049e89c33da4df488f5c4a69
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2025-05-26 21:18:38 +0530

    main

--- syncial/pages/index.js
+++ syncial/pages/index.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import Image from "next/image";
+import logo from '@/public/logo.jpg'
 
 export default function LandingPage() {
   useEffect(() => {
@@ -12,7 +14,11 @@ export default function LandingPage() {
       {/* NAVBAR */}
       <nav className="flex justify-between items-center px-8 md:px-24 py-6 bg-black">
         <Link href='/'>
-        <h1 className="text-3xl font-extrabold tracking-tight">Syncial</h1></Link>
+        <Image
+                    src = {logo}
+                    alt=''
+                 width="230"
+                 height="230"/></Link>
         <div className="space-x-6 text-md">
           {/* <Link href="#features" className="hover:text-blue-400 transition">Features</Link>
           <Link href="#how-it-works" className="hover:text-blue-400 transition">How It Works</Link> */}

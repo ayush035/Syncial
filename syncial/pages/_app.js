58396ab4ad729bc7811111a6e52e00ac603b632f
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2025-05-26 21:47:42 +0530

    analytics

--- syncial/pages/_app.js
+++ syncial/pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 // import Footer from '../components/Footer';
 import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/next';
 
 import {
   RainbowKitProvider,
@@ -42,6 +43,7 @@ function MyApp({ Component, pageProps }) {
         <div className="flex flex-col min-h-screen">
           <div className="flex-grow">
             <Component {...pageProps} />
+            <Analytics />
           </div>
           {/* <Footer /> */}
         </div>

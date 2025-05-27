1ca03f4de76f49ff5290b6c8e6fb430072c3c2a2
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2025-05-27 19:50:57 +0530

    main

--- syncial/components/Navbar.js
+++ syncial/components/Navbar.js
@@ -3,6 +3,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 // import  '@/styles/Home.module.css'
 import Image from "next/image";
 import logo from '@/public/logo.jpg'
+import { Search } from "lucide-react";
 import { Input } from "postcss";
 export default function Navbar() {
 
@@ -21,8 +22,15 @@ export default function Navbar() {
                     alt=''
                  width="230"
                  height="230"/></Link>
+                   <div className="flex items-center bg-[#16030d] rounded-xl w-fit my-4 mx-6 px-4 h-11 outline outline-1 outline-[#39071f]">
+      <Search className="text-[#563e4b] w-5 h-5 mr-2" />
+      <input
+        className="text-rose-100 bg-[#16030d] text-lg w-60 h-full outline-none "
+        placeholder="Search"
+      />
+    </div>
                  
-                     <input className="rounded-xl text-rose-100  py-2 px-4 bg-[#16030d] w-60 h-11 text-lg outline-1 outline-[#39071f] my-4 mx-6" placeholder=" 🔍  Search"/>
+                     {/* <input className="rounded-xl text-rose-100  py-2 px-4 bg-[#16030d] w-60 h-11 text-lg outline-1 outline-[#39071f] my-4 mx-6" placeholder=" 🔍  Search"/> */}
                      </div>
         <div className="space-x-6 text-md flex ">
           {/* <Link href="#features" className="hover:text-blue-400 transition">Features</Link>
